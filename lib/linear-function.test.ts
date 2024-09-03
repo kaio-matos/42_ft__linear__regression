4a1f1fb53c6af01e0f1fe641c50a55388a37b02c
Kaio Matos <kaio_matos22@hotmail.com>
2024-09-02 22:05:57 -0300

    feat: linear-function classes

---
+++ lib/linear-function.test.ts
@@ -0,0 +1,43 @@
+import { expect, test, describe } from "bun:test";
+import {
+  LinearMapLinearFunction,
+  PolymonialLinearFunction,
+} from "./linear-function";
+import { Vector2D } from "./vector-2d";
+
+describe("PolymonialLinearFunction", () => {
+  test("positive gradient -> slope = 0.5; intercept = 2; x = 10", () => {
+    const positiveGradientFunction = PolymonialLinearFunction.create()
+      .slope(0.5)
+      .intercept(2);
+
+    expect(positiveGradientFunction.generate(10)).toBe(7);
+  });
+
+  test("negative gradient -> slope = -1; intercept = 5; x = 10", () => {
+    const negativeGradientFunction = PolymonialLinearFunction.create()
+      .slope(-1)
+      .intercept(5);
+    expect(negativeGradientFunction.generate(10)).toBe(-5);
+  });
+});
+
+describe("LinearMapLinearFunction", () => {
+  test("should invert the vector components", () => {
+    const linearMapFunction = LinearMapLinearFunction.create()
+      .xTransformer((x, y) => y)
+      .yTransformer((x, y) => x);
+
+    const result = linearMapFunction.generate(Vector2D.create([13, 23]));
+    expect(result).toEqual(Vector2D.create([23, 13]));
+  });
+
+  test("should invert the sign of each component", () => {
+    const linearMapFunction = LinearMapLinearFunction.create()
+      .xTransformer((x, y) => -x)
+      .yTransformer((x, y) => -y);
+
+    const result = linearMapFunction.generate(Vector2D.create([-100, 233]));
+    expect(result).toEqual(Vector2D.create([100, -233]));
+  });
+});
---
+++ lib/linear-function.ts
@@ -0,0 +1,68 @@
+/**
+ * @link https://en.wikipedia.org/wiki/Linear_function
+ */
+
+import { Vector2D } from "./vector-2d";
+
+// f(x) = ax + b
+export class PolymonialLinearFunction {
+  /*
+    In calculus, analytic geometry and related areas,
+    a linear function is a polynomial of degree one or less
+  */
+
+  private __slope = 0;
+  private __intercept = 0;
+
+  static create() {
+    return new PolymonialLinearFunction();
+  }
+
+  slope(a: number) {
+    this.__slope = a;
+    return this;
+  }
+
+  intercept(b: number) {
+    this.__intercept = b;
+    return this;
+  }
+
+  generate(x: number) {
+    return this.__intercept + this.__slope * x;
+  }
+}
+
+type LinearMapComponentTransformer = (x: number, y: number) => number;
+export class LinearMapLinearFunction {
+  /*
+    In linear algebra, a linear function is a map `f` between
+    two vector spaces
+  */
+  private __xTransformer: LinearMapComponentTransformer = (x, y) => x;
+  private __yTransfomer: LinearMapComponentTransformer = (x, y) => y;
+
+  static create() {
+    return new LinearMapLinearFunction();
+  }
+
+  xTransformer(x: LinearMapComponentTransformer) {
+    this.__xTransformer = x;
+    return this;
+  }
+
+  yTransformer(y: LinearMapComponentTransformer) {
+    this.__yTransfomer = y;
+    return this;
+  }
+
+  generate(v: Vector2D) {
+    const result = Vector2D.create();
+    result.push(
+      this.__xTransformer(v[0], v[1]),
+      this.__yTransfomer(v[0], v[1]),
+    );
+
+    return result;
+  }
+}
